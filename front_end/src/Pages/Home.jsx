9ddcc4128d4eabf124b891c3d18ffb17192cefff
dennea <dennea1228@gmail.com>
2023-10-11 22:10:48 -0600

    started create post form

--- front_end/src/Pages/Home.jsx
+++ front_end/src/Pages/Home.jsx
@@ -3,13 +3,25 @@ import { Box, Stack, Skeleton } from "@mui/material";
 import Post from '../Components/post/Post';
 import { useState } from "react";
 import { tempPosts } from './tempPosts';
+import Fab from '@mui/material/Fab';
+import AddIcon from '@mui/icons-material/Add';
+import CreatePost from '../Components/createpost/CreatePost';
 
 function Home() {
   const [loading, setLoading] = useState(true);
+  const [isCreateModalOpen, setIsCreateModalOpen] = React.useState(false);
+
+  const openCreateModal = () => {
+    setIsCreateModalOpen(true);
+  };
+
+  const closeCreateModal = () => {
+    setIsCreateModalOpen(false);
+  };
 
   setTimeout(() => {
     setLoading(false);
-  }, [3000]);
+  }, [1000]);
 
   return (
     <Box px={{ md: 22 }}>
@@ -34,6 +46,14 @@ function Home() {
         })}
         </>
       )}
+      <Fab 
+        color="primary" 
+        aria-label="add"
+        style={{position: 'fixed', top: '2rem', right: '45rem'}}
+        onClick={openCreateModal}>
+        <AddIcon />
+      </Fab>
+      <CreatePost open={isCreateModalOpen} onClose={closeCreateModal} action='CREATE'/>
     </Box>
   );
 }
