24e0d980c718a497e16620e9710bd711bf05aae8
Julian Gallego Franco <jcgallego22@gmail.com>
2023-10-12 21:00:06 -0600

    Adding friends

--- front_end/src/Pages/Friends.jsx
+++ front_end/src/Pages/Friends.jsx
@@ -1,10 +1,52 @@
-import React from 'react';
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { tempAuthors } from './tempAuthors';
+import * as bootIcons from "react-icons/bs";
+import AuthorTile from '../Components/friends/authorTile';
 
 function Friends() {
   return (
-    <div className='friends'>
-      <h1>Friends</h1>
-    </div>
+    <Box px={{ md: 22 }} sx={{paddingTop: "10px"}}>
+    <AppBar position="static" sx={{ backgroundColor: '#3a86ff' }}>
+      <Container maxWidth="md">
+        <Toolbar disableGutters>
+          <bootIcons.BsPeopleFill style={{ fontSize: '1.5rem' }}/>
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              ml: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontSize: "20px",
+              fontWeight: 700,
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            Friends
+          </Typography>
+        </Toolbar>
+      </Container>
+    </AppBar>
+    <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+    {tempAuthors.map((post, index) =>{
+          return (
+            <AuthorTile 
+              username={post.name} 
+              profilePic={post.profile}
+              status={post.status}
+              />
+          )
+        })}
+      </Box>
+    </Box>
   );
 }
 
---
+++ front_end/src/Pages/tempAuthors.js
@@ -0,0 +1,19 @@
+import Profile from '../Images/profile.png'
+
+export const tempAuthors = [
+    {
+      name: 'Selena Gomez',
+      profile: Profile,
+      status: 'followable'  
+    },
+    {
+      name: 'Selena Gomez',
+      profile: Profile,
+      status: 'pending'  
+    },
+    {
+      name: 'Selena Gomez',
+      profile: Profile,
+      status: 'friends' 
+    },
+  ];
