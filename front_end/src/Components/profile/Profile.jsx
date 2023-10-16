ddeccd6fbbe605a1807dba8d386a132032b42e23
King-Sly <nkokeke@ualberta.ca>
2023-10-16 16:24:48 -0600

    Removed eslint

--- front_end/src/Components/profile/Profile.jsx
+++ front_end/src/Components/profile/Profile.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import UserCard from "./UserCard";
 import { tempPosts } from "../../Pages/tempPosts";
 import Post from "../post/Post";
@@ -6,42 +6,44 @@ import { Fab } from "@mui/material";
 import AddIcon from '@mui/icons-material/Add';
 import CreatePost from "../createpost/CreatePost";
 
-function ProfilePage({isOwner = true}) {
+function ProfilePage({ isOwner = true }) {
+    const [isCreateModalOpen, setIsCreateModalOpen] = useState(false);
 
-    const [isCreateModalOpen, setIsCreateModalOpen] = React.useState(false);
-    
     const openCreateModal = () => {
         setIsCreateModalOpen(true);
     };
-    
+
     const closeCreateModal = () => {
-    setIsCreateModalOpen(false);
+        setIsCreateModalOpen(false);
     };
 
     return (
         <>
-            <UserCard isOwner = {isOwner} />
+            <UserCard isOwner={isOwner} />
             <div>
-                {tempPosts.map((post, index) =>{
-                    return (
-                        <Post 
-                            username={post.name} 
-                            profilePic={post.profile}
-                            photo={post.photo}
-                            text={post.text}
-                            visibility={post.visibility}/>
-                    )
-                    })}
+                {tempPosts.map((post, index) => (
+                    <Post
+                        key={index}
+                        username={post.name}
+                        profilePic={post.profile}
+                        photo={post.photo}
+                        text={post.text}
+                        visibility={post.visibility}
+                    />
+                ))}
             </div>
-            <Fab 
-                color="primary" 
-                aria-label="add"
-                style={{position: 'fixed', top: '20rem', right: '5rem'}}
-                onClick={openCreateModal}>
-            <AddIcon />
-      </Fab>
-      <CreatePost open={isCreateModalOpen} onClose={closeCreateModal} action='CREATE'/>
-    </>
+            {isOwner ? (
+                <Fab
+                    color="primary"
+                    aria-label="add"
+                    style={{ position: 'fixed', top: '20rem', right: '5rem' }}
+                    onClick={openCreateModal}
+                >
+                    <AddIcon />
+                </Fab>
+            ) : null}
+            {isCreateModalOpen && <CreatePost open={isCreateModalOpen} onClose={closeCreateModal} action='CREATE' />}
+        </>
     );
 }
 
