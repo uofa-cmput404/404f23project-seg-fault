4e58e7f26058d6fbcb830770d4be185854d40c96
Julian Gallego Franco <jcgallego22@gmail.com>
2023-10-22 13:10:06 -0600

    Adding friends and follow/unfollow.

--- front_end/src/Components/friends/authorTile.jsx
+++ front_end/src/Components/friends/authorTile.jsx
@@ -1,49 +1,45 @@
-import React from 'react';
-import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
-import { Chip } from '@mui/material';
-import Avatar from '@mui/material/Avatar';
-import ButtonBase from '@mui/material/ButtonBase';
+import React from "react";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import { Chip } from "@mui/material";
+import Avatar from "@mui/material/Avatar";
+import ButtonBase from "@mui/material/ButtonBase";
 
 function AuthorTile(props) {
-
   const chipLabels = {
-    friends: 'Friends',
-    followable: 'Follow',
-    pending: 'Following',
+    follow: "Follow",
+    unfollow: "Unfollow",
+    follower: "Following you",
+    friend: "Remove friend",
   };
 
   const chipColors = {
-    friends: '#8338ec',
-    followable: '#3a86ff',
-    pending: '#ff006e',
+    follow: "#3a86ff",
+    unfollow: "#e63946",
+    follower: "#8338ec",
+    friend: "#e63946",
   };
 
-  const chipColor = chipColors[props.status] || '#000';
+  const chipColor = chipColors[props.status] || "#000";
 
   const handleClick = () => {
     props.authorAction(props.id);
   };
+
   return (
     <Card className="card">
       <CardHeader
-        avatar={
-          <Avatar
-            src={props.profilePic}
-            alt=""
-            className="avatar"
-          />
-        }
+        avatar={<Avatar src={props.profilePic} alt="" className="avatar" />}
         title={props.username}
         action={
           <ButtonBase onClick={handleClick}>
             <Chip
               label={chipLabels[props.status]}
               size="small"
-              className='postVisibility'
+              className="postVisibility"
               sx={{ paddingRight: 2, marginTop: 1.5 }}
               color="primary"
-              style={{ backgroundColor: chipColor, color: '#fff' }}
+              style={{ backgroundColor: chipColor, color: "#fff" }}
             />
           </ButtonBase>
         }
