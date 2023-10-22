d72f4e454fb4c1ad9d0454a750b05625842d6d7f
dennea <dennea1228@gmail.com>
2023-10-22 16:25:36 -0600

    fixed posts and added update posts

--- front_end/src/Components/postMenu/PostMenu.jsx
+++ front_end/src/Components/postMenu/PostMenu.jsx
@@ -108,7 +108,7 @@ export default function PostMenu(props) {
           )}
         </Popper>
       </div>
-      <CreatePost open={isEditModalOpen} onClose={closeEditModal}  action='EDIT'/>
+      <CreatePost open={isEditModalOpen} onClose={closeEditModal}  action='EDIT' post={props.post}/>
     </>
   );
 }
