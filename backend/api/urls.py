6cb22ee010ea5e785ec53364385bf20a188507fe
Kourosh Kehtari <kouroshkehtari@gmail.com>
2023-10-30 01:05:14 -0600

    docs

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -9,6 +9,25 @@ from .inbox.inboxViews import InboxView, PostLikesListView
 
 from .views import FollowAuthorView, UnfollowAuthorView, FollowersListView, FollowingListView, FollowerView, CommentListView
 
+from django.urls import path, re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="",
+        default_version='v1',
+        description="",
+        terms_of_service="",
+        contact=openapi.Contact(email=""),
+        license=openapi.License(name=""),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='user-registration'),
     path('login/', UserLoginView.as_view(), name='user-login'),
@@ -29,4 +48,9 @@ urlpatterns = [
     path('authors/<str:author_id>/inbox/', InboxView.as_view(), name='inbox_create'),
     ## Likes
     path('authors/<str:author_id>/posts/<str:post_id>/likes/', PostLikesListView.as_view(), name='post-likes'),
+    ##docs
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
 ]
