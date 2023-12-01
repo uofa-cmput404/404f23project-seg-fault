292d04fe118a610a934f925e7d0d7f13dab211a2
kooner27 <prabhnoo@ualberta.ca>
2023-11-30 21:23:09 -0700

    make it better

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Author, Post, Comment, Like, Inbox, AuthorFollower, FollowRequest
-
+from .models import Author, Post, Comment, Like, Inbox, AuthorFollower
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('displayName', 'type', 'url', 'user', 'id', 'host', 'github', 'profileImage')
 
